c6222391e7512f4ada1bc841978429c93ed78627
feifeiwoye <feifeiwoye@outlook.com>
2018-03-21 10:40:43 +0800

    mandatory jump to change password.

--- skycoin-messenger/monitor/web/src/app/page/update-pass/update-pass.component.ts
+++ skycoin-messenger/monitor/web/src/app/page/update-pass/update-pass.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from '../../service';
 import { Router } from '@angular/router';
 import swal from 'sweetalert2';
+import { error } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-updatepass-page',
@@ -17,6 +18,7 @@ export class UpdatePassComponent implements OnInit {
     newpass: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]),
   });
   status = 0;
+  serverHint: any = 'Please confirm that the original password is correct and then try to update again.';
   constructor(private api: ApiService, private router: Router) { }
 
   ngOnInit() {
@@ -34,7 +36,7 @@ export class UpdatePassComponent implements OnInit {
     data.append('oldPass', this.updateForm.get('oldpass').value);
     data.append('newPass', this.updateForm.get('newpass').value);
     this.api.updatePass(data).subscribe(result => {
-      if (result) {
+      if (result === 'true') {
         swal({
           title: 'Warning',
           text: 'The password has been changed. Click the button to jump to the login page.',
@@ -44,9 +46,13 @@ export class UpdatePassComponent implements OnInit {
         }).then(() => {
           this.router.navigate([{ outlets: { user: ['login'] } }]);
         });
+      } else {
+        this.status = 1;
+        this.serverHint = result;
       }
     }, err => {
       this.status = 1;
+      this.serverHint = err;
     });
   }
 }
