c8cfbe5a0f03a2cad42f92926d24007b22a40717
Ivan Costa Lacuesta <icosta@tribeclick.com>
2018-08-12 21:59:05 +0200

    removed vendor

--- vendor/github.com/skycoin/skycoin/src/gui/static/src/app/directives/dontsavepassword.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DontsavepasswordDirective } from './dontsavepassword.directive';
-
-describe('DontsavepasswordDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DontsavepasswordDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- vendor/github.com/skycoin/skycoin/src/gui/static/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  nodeUrl: 'http://127.0.0.1:6420/',
-  production: true,
-  tellerUrl: 'https://event.skycoin.net/api/',
-};
